24da3f51a2139adc0b303a5ff1058ee912fc62a9
Mykola Klymchuk <m.klymchuk@student.po.edu.pl>
2020-10-09 01:00:41 +0200

    Add project files.

---
+++ Program 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pierwszy program");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+
+           for(int i = 1; i < 10; i++)
+            {
+                Console.WriteLine("Linijka {0}", i);
+               // Console.WriteLine("Linijka " + i);
+            }
+            przykladowametoda();
+        }
+        static void przykladowametoda()
+        {
+            Console.Beep(2000, 3000);
+        }
+    }
+}
